aa59e39eb1e9c85922aca3566381a471beb986a0
Mihai <stmihai98@gmail.com>
2023-09-18 00:26:44 +0300

    feat: add skaffold and client

---
+++ client/src/CommentCreate.js
@@ -0,0 +1,36 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+
+    await axios.post(`http://posts.com/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group" style={{ paddingBottom: '0.625rem' }}>
+          <label>New Comment</label>
+          <input
+            value={content}
+            onChange={(e) => {
+              setContent(e.target.value);
+            }}
+            className="form-control"
+          />
+        </div>
+        <button className="btn btn-primary">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
